86bd7a55c983d2c4fef7126e0add30e445bfcdf8
austriker27 <davidalindahl@gmail.com>
2017-08-14 14:35:18 -0700

    working on object for shops

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
 'use strict';
+
+var cookieShops = {
+  shop: '1st and Pike',
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookies: 6.3,
+  randomCustomers: Math.floor(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers),
+  cookiesPerHour: this.avgCookies * this.randomCustomers,
+}
+
+  //
+  // genRandomCust: function () {
+  //   Math.floor(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers);
+  // }
+  //
+  // cookiesPerHour: function () {
+  //   this.avgCookies * genRandomCust;
+
+  }
+};
+
+
+var cookieShops = {
+  shop: 'SeaTac Airport',
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgCookies: 1.2,
+
+
+
+var cookieShops = {
+  shop: 'Seattle Center',
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgCookies: 3.7,
+
+
+var cookieShops = {
+  shop: 'Capitol Hill',
+  minCustomers: 20,
+  maxCustomers: 38,
+  avgCookies: 2.3,
+
+var cookieShops = {
+  shop: 'Alki',
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgCookies: 4.6,
